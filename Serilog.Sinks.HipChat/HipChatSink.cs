c521e2ea5223db88927d944dece2e6f96678d0d9
Adel Refaat <adelkamel@gmail.com>
2016-04-26 17:28:35 +1000

    Initial commit

---
+++ Serilog.Sinks.HipChat/HipChatSink.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HipChat.Net;
+using HipChat.Net.Http;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.HipChat
+{
+    internal class HipChatSink : ILogEventSink, IDisposable
+    {
+        private readonly string _roomId;
+        private readonly LogEventLevel _restrictedToMinimumLevel;
+
+        private readonly HipChatClient _client;
+        private readonly object _syncRoot = new object();
+
+        public HipChatSink(string token, string roomId, LogEventLevel restrictedToMinimumLevel)
+        {
+            _roomId = roomId;
+            _restrictedToMinimumLevel = restrictedToMinimumLevel;
+            _client = new HipChatClient(new ApiConnection(new Credentials(token)));
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent.Level < _restrictedToMinimumLevel)
+                return;
+
+            lock (_syncRoot)
+            {
+                var message = logEvent.RenderMessage();
+                _client.Rooms.SendNotificationAsync(_roomId, message);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Serilog.Sinks.HipChat/LoggerSinkConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Events;
+
+namespace Serilog.Sinks.HipChat
+{
+    public static class LoggerSinkConfigurationExtensions
+    {
+        public static LoggerConfiguration HipChat(this LoggerSinkConfiguration sinkConfiguration, string token, string roomId, LogEventLevel restrictedToMinimumLevel = LogEventLevel.Error, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}")
+        {
+            if (sinkConfiguration == null)
+            {
+                throw new ArgumentNullException("sinkConfiguration");
+            }
+            if (outputTemplate == null)
+            {
+                throw new ArgumentNullException("outputTemplate");
+            }
+            var hipChatSink = new HipChatSink(token, roomId, restrictedToMinimumLevel);
+            return sinkConfiguration.Sink(hipChatSink, restrictedToMinimumLevel);
+        }
+    }
+}
