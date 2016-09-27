52d95721c09e19e5759e2251e19a511f76c50ff3
Adel Refaat <adelkamel@gmail.com>
2016-09-27 18:36:46 +1000

    Updating nuget packages

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -8,10 +8,12 @@ namespace ConsoleApplication
     {
         static void Main()
         {
-            const string token = "Token Here";
-            const string roomId = "RoomId Here";
-            var log = new LoggerConfiguration().WriteTo.HipChat(token, roomId , LogEventLevel.Verbose, 1, 1).CreateLogger();
-            log.Error("Hello !");
+            const string token = "Token goes here";
+            const string roomId = "Room Id goes here";
+            using (var log = new LoggerConfiguration().WriteTo.HipChat(token, roomId, LogEventLevel.Verbose, 1, 1).CreateLogger())
+            {
+                log.Error("Hello !");
+            }
         }
     }
 }
